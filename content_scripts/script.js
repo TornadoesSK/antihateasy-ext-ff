87446083d8fb34d5cb147f7a4f7be9420aac042c
Ivan Havasi <ivohavasi@gmail.com>
2022-04-24 01:16:08 +0200

    Bla bla

--- content_scripts/script.js
+++ content_scripts/script.js
@@ -1,9 +1,54 @@
 var isOn = false;
 var counter = 0;
 
+function blurElement(elem) {
+  // setting article style
+  elem.classList.add("antihate-blurred");
+  elem.style.filter = "blur(5px)";
+  // setting parent element
+  var newElem = document.createElement("div");
+  newElem.classList.add("antihate-blur");
+  newElem.style.position = "absolute";
+  newElem.style.display = "flex";
+  newElem.style.width = "100%";
+  newElem.style.height = "100%";
+  newElem.style.justifyContent = "center";
+  newElem.style.alignItems = "center";
+  newElem.style.zIndex = "100";
+  // setting inside element
+  var inside = document.createElement("p");
+  inside.innerText = "Hateful speech blocked!";
+  inside.style.backgroundColor = "black";
+  inside.style.border = "2px solid black";
+  inside.style.borderRadius = "10px";
+  inside.style.fontSize = "1.2em";
+  inside.style.opacity = "0.6";
+  inside.style.padding = "5px";
+  inside.style.color = "white";
+  newElem.appendChild(inside);
+  // append before article
+  elem.before(newElem);
+}
+
 // determine if elem is hate speech
 function isHateSpeech(elem) {
   data = elem.querySelector("div[lang]")
+  console.log("TOTO SU DATA")
+  console.log(data)
+  bodyData = {"message": data.textContent}
+  console.log("IS HATE? " + data.textContent)
+  console.log(JSON.stringify(bodyData))
+  fetch("https://antihate.free.beeceptor.com/api/message/hate", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(bodyData)
+  }).then(res => {
+    console.log("Request complete! response:", res);
+  }).catch(err => {
+    console.log("ERRRPR")
+    console.log(err);
+  });
+
   if (counter % 3 == 0) {
     return true;
   } else {
@@ -30,31 +75,7 @@ function main() {
   for (const elem of articles) {
     // blur if is hate speech
     if (isHateSpeech(elem)) {
-        // setting article style
-        elem.classList.add("antihate-blurred");
-        elem.style.filter = "blur(5px)";
-        // setting parent element
-        var newElem = document.createElement("div");
-        newElem.classList.add("antihate-blur");
-        newElem.style.position = "absolute";
-        newElem.style.display = "flex";
-        newElem.style.width = "100%";
-        newElem.style.height = "100%";
-        newElem.style.justifyContent = "center";
-        newElem.style.alignItems = "center";
-        newElem.style.zIndex = "100";
-        // setting inside element
-        var inside = document.createElement("p");
-        inside.innerText = "Hateful speech blocked!";
-        inside.style.backgroundColor = "black";
-        inside.style.border = "2px solid black";
-        inside.style.borderRadius = "10px";
-        inside.style.fontSize = "1.2em";
-        inside.style.opacity = "0.6";
-        inside.style.padding = "5px";
-        newElem.appendChild(inside);
-        // append before article
-        elem.before(newElem);
+        blurElement(elem)
     }
     counter += 1
   } 
